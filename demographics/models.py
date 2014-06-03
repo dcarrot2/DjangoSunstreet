d435fca46446bc5dcd4c8f35166b01a6c3307116
Edgar Sandoval <hesandova@gmail.com>
2014-06-03 15:58:34 -0700

    Added Zipcodes to DB and removed the Person model attributes

--- demographics/models.py
+++ demographics/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Zipcode(models.Model):
+    zipcode = models.CharField(max_length=10)
+    count = models.IntegerField(default=0)
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.zipcode
+
+class Person(models.Model):
+    #age = models.CharField(max_length = 10)
+    #ageCount = models.IntegerField(default=0)
+    #MaleOrFemale = models.CharField(max_length = 10)
+    #maleCount = models.IntegerField(default = 0)
+    #femaleCount = models.IntegerField(default = 0)
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.age
+
