3fa50c9049b144420e43265bc304ddf948d55883
Edgar Sandoval <hesandova@gmail.com>
2014-06-04 18:04:52 -0700

    Added Script to add the gender, age, and zipcode for users Changed the graph to show the information for the zipcode of users

--- demographics/models.py
+++ demographics/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-
+#model stores the zipcode and stores the count of the zipcode
 class Zipcode(models.Model):
     zipcode = models.CharField(max_length=10)
     count = models.IntegerField(default=0)
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.zipcode
-
+#model stores the age range and stores the count of the age range
 class Age(models.Model):
     age = models.CharField(max_length = 10)
     ageCount = models.IntegerField(default=0)
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.age
-
+#model stores the two possible genders and keeps count of the user that select that gender
 class Gender(models.Model):
     gender = models.CharField(max_length = 10)
     count = models.IntegerField(default = 0)
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.gender
-
+#model stores the specific user, gives it a value beginning at 1 then stores the users zip, age, and gender
 class User(models.Model):
     zip = models.ForeignKey(Zipcode)
     age = models.ForeignKey(Age)
---
+++ extractZip.py
@@ -0,0 +1,55 @@
+import os
+import urllib2
+from BeautifulSoup import BeautifulSoup
+def populate():
+    url = "http://www.mapszipcode.com/california/county/monterey"
+    f = urllib2.urlopen(url).read()
+
+    zipList = []
+
+    soup = BeautifulSoup(f)
+
+    for x in soup.findAll('a'):
+        try:
+            if(str(''.join(x.findAll(text=True)))[0] == "9"):
+                zipList.append(str(''.join(x.findAll(text=True))[0:5]))
+        except:
+            print "Exception"
+    for i in zipList:
+        zipCodeModel = Zipcode.objects.get_or_create(zipcode=i)
+    addGender("Male")
+    addGender("Female")
+    addAgeRange("9-13")
+    addAgeRange("14-18")
+    addAgeRange("19+")
+
+    for a in Age.objects.all():
+        print str(a)
+    for a in Gender.objects.all():
+        print str(a)
+    for a in Zipcode.objects.all():
+        print str(a), " ",
+
+def addGender(gen):
+    genderModel = Gender.objects.get_or_create(gender = gen)
+    return genderModel
+def addAgeRange(range):
+    ageRangeObject = Age.objects.get_or_create(age=range)
+    return ageRangeObject
+
+
+if __name__ == '__main__':
+	print "Adding information for Zipcodes, Gender, and Ages"
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE',"sunstreet.settings")
+	from demographics.views import Age, Zipcode, Gender
+	populate()
+
+#print
+#print "[",
+#for x in zipList:
+#    print x +", ",
+#print"]"
+#print len(zipList)
+
+#print soup.prettify()
+
