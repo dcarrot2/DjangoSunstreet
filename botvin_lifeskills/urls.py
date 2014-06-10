0a65875659b3f440804c091adb32cef2b81f9d4b
Edgar Sandoval <hesandova@gmail.com>
2014-06-09 20:23:50 -0700

    URL, Views

--- botvin_lifeskills/urls.py
+++ botvin_lifeskills/urls.py
@@ -5,5 +5,6 @@ from botvin_lifeskills import views
 urlpatterns = patterns('',
 
 	url(r'^section/(?P<section>\D+)/(?P<school_level>\D+)$', views.botvinSection, name='sectionAHSBotvin'),
+    url(r'^section/(?P<section>\D+)/(?P<school_level>\D+)/vote$', views.botvinSectionVote, name='botvinsectionvote'),
 
 )
