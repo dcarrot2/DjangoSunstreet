d9237dc370778e2180e069617ee1db62bd31887c
Edgar Sandoval <hesandova@gmail.com>
2014-06-10 14:34:51 -0700

    Finally got the URL redirect to work, using hidden type variables within the html

--- botvin_lifeskills/urls.py
+++ botvin_lifeskills/urls.py
@@ -4,8 +4,10 @@ from botvin_lifeskills import views
 
 urlpatterns = patterns('',
 
-	url(r'^section/(?P<section>\D+)/(?P<school_level>\D+)$', views.botvinSection, name='sectionAHSBotvin'),
+	url(r'^section/(?P<section>\D+)/(?P<school_level>\D+)$', views.botvinSection, name='botvinsurvey'),
 
-    url(r'^section/(?P<section>\D+)/(?P<school_level>\D+)/botvinSectionVote/$', views.botvinSectionVote, name="botvinSectionVote"),
+    url(r'^section/botvinVote/$', views.botvinSectionVote, name="botvinSectionVote"),
+
+    url(r'^section/results/$', views.results, name="results"),
 
 )
--- botvin_lifeskills/views.py
+++ botvin_lifeskills/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from botvin_lifeskills.models import Question, Answer, Botvin_Section, User
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 # Create your views here.
 
 def botvinSection(request, section, school_level):
             #We get the first two questions individually given
         #that they require a text field for response
+    print "section: ", section
+    print "school level", school_level
     questions = []
     for question in Question.objects.get_queryset().filter(section_letter = section).filter(school_level = school_level):
         questions.append(question)
@@ -17,19 +19,27 @@ def botvinSection(request, section, school_level):
     # for i in range(0, len(questions)):
     #     context["question_"+ str(i+1)] = questions[i]
     #
+    # print questions
     context["questions"] = questions
     context["section"] = section
     context["school_level"] = school_level
-    print context
+
+    print "printing the context", context
     return render(request, "botvin/displayquestions.html", context)
 
-def botvinSectionVote(request, section, school_level):
+def botvinSectionVote(request):#, section, school_level):
     questions = []
-    for question in Question.objects.get_queryset().filter(seciton_letter=section).filter(school_level=school_level):
-        questions.append(question)
+    print "123456789"
+    print "request.post['1']", request.POST['1']
+    print "\n\n"
+    print Answer.objects.get(pk=1), "\n\n"
+    print request
+    questions = get_list_or_404(Question, section_letter = request.POST['section'], school_level = request.POST['school_level'])
+    # print questions[0].answer_set.all()
     try:
         responses = []
         for i in range(0,len(questions)):
+            print(Answer.objects.get(pk=request.POST['choice']))
             responses.append(Answer.objects.get(pk = request.POST['choice'+str(i+1)]))
         for i in range(0, len(responses)):
             print "Response " + str(i+1)+":" + responses[i]
@@ -40,6 +50,11 @@ def botvinSectionVote(request, section, school_level):
 		'error_message': "You forgot to select one or more choices."})
 
     return HttpResponseRedirect(reverse('lifeskills:response'))
+
+def results(request):
+    print "123456"
+    return HttpResponse("Hello World")
+
 
 
 
