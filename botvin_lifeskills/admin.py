abcb8170b60f9aaa9378196878896dc35a815c1c
Daniel Diaz <diazjfdaniel@gmail.com>
2014-06-25 09:53:10 -0700

    Fixed admin module

--- botvin_lifeskills/admin.py
+++ botvin_lifeskills/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from botvin_lifeskills.models import Answer, Question, User, Botvin_Section
 class UserAdmin(admin.ModelAdmin):
 
-    list_display = ['student_code', 'school_code', 'date_survey_taken', 'section_a_questions', "section_b_questions",
+    list_display = ['school_code', 'date_survey_taken', 'section_a_questions', "section_b_questions",
                     'section_c_questions', 'section_d_questions']
-    list_display = ['student_code', 'school_code',]
+    list_display = ['school_code']
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['question_number', 'question', 'section', 'school_level']
