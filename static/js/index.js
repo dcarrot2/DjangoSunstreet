96788583d44724518afd672db99e296efdcb8138
Daniel Diaz <diazjfdaniel@gmail.com>
2014-07-19 14:00:27 -0700

    Fixed index.js to send input values if school level and school name are choosen

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 	
-	var input = document.getElementById("schoolform");
+
 	$("input").click(function(){
-	
+		
 		var e = document.getElementById("item1");
 		if($(e).val() == ""){
 			alert("You forgot to choose a school!");
 			return false;
 		}
+		else{
+
+			document.school_select_form.action = document.school_select_form.action + event.target.id;
+			return true;
+		}
+
+
 	});
 });
